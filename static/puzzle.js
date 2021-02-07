71b2d75aec9456afd390e31a143d1d4410083802
Chris Hopkins <cbehopkins@gmail.com>
2021-02-07 17:11:43 +0000

    Working version of countdown

--- static/puzzle.js
+++ static/puzzle.js
@@ -8,6 +8,15 @@ const anagramRow = document.getElementById("anagram-input");
 const anagramBtn = document.getElementById("anagram-btn");
 const anagramInput = document.getElementById("anagram-get-it-here");
 
+const countdownRow = document.getElementById("countdown-input");
+const countdownBtn = document.getElementById("countdown-btn");
+const countdownTable = document.getElementById("countdown-get-it-here");
+const countdownTarget = document.getElementById('countdown-target-input');
+
+const sudokuRow = document.getElementById("sudoku-input");
+const sudokuBtn = document.getElementById("sudoku-btn");
+const sudokuTable = document.getElementById("sudoku-get-it-here");
+
 const resultRow = document.getElementById("result-row");
 loadEventListeners();
 
@@ -17,10 +26,17 @@ function loadEventListeners(){
     selectSudokuMenu.addEventListener('click', selectSudoku);
 
     anagramBtn.addEventListener('click', calculateAnagram);
+    countdownBtn.addEventListener('click', calculateCountdown);
+    sudokuBtn.addEventListener('click', calculateSudoku);
+
 }
 function hideAllViews(){
-    [resultRow, anagramRow].forEach(x => x.style="display:none");
+    [resultRow, anagramRow, countdownRow, sudokuRow].forEach(x => x.style="display:none");
 }
+
+///////////////////////////////////////
+// Select the puzzle in quesiotn and display
+///////////////////////////////////////
 function selectAnagram (e) {
     hideAllViews();
     console.log("Select the anagram");
@@ -28,6 +44,25 @@ function selectAnagram (e) {
 
     e.preventDefault();
 }
+
+function selectCountdown (e) {
+    hideAllViews()
+    console.log("Select the countdown");
+    inputTablePopulate(countdownTable, 6, 1, "countdown", "number")
+    countdownRow.style= ""; // Display the page
+    e.preventDefault();
+}
+function selectSudoku (e) {
+    hideAllViews();
+    console.log("Select the sudoku");
+    inputTablePopulate(sudokuTable, 9, 9, "sudoku", "number")
+    sudokuRow.style= ""; // Display the page
+    e.preventDefault();
+}
+
+///////////////////////////////////////
+// Run the Calculations
+///////////////////////////////////////
 function calculateAnagram(e){
     returnStruct = anagram(anagramInput.value);
     console.log(returnStruct)
@@ -39,17 +74,41 @@ function calculateAnagram(e){
     populateTab(roomResultList, resultArray);
     e.preventDefault();
 }
+function calculateCountdown(e){
+    // The first input number is always the target
+    // let testInput = ["4", "5", "10", "100", "50", "1"]
+    let inputTable = inputTableRetrieve(countdownTable); 
 
-function selectCountdown (e) {
-    hideAllViews()
-    console.log("Select the countdown");
+    // We want the first (and only) row of the table
+    returnStruct = countdown(countdownTarget.value, JSON.stringify(inputTable[0]));
+    console.log(returnStruct)
+    if (returnStruct["error"] != "") {
+        console.log(`Got an error ${returnStruct["err"]}`)
+        return;
+    }
+    let rs = returnStruct["countdown"]
+    console.log(`Result:${rs}`)
+    // resultArray = rePopulate(returnStruct["countdown"], 3);
+    // populateTab(roomResultList, resultArray);
     e.preventDefault();
 }
-function selectSudoku (e) {
-    hideAllViews();
-    console.log("Select the sudoku");
+function calculateSudoku(e){
+    inputTable = inputTableRetrieve(sudokuTable);
+    returnStruct = sudoku(inputTable);
+    console.log(returnStruct)
+    if (returnStruct["error"] != "") {
+        console.log(`Got an error ${returnStruct["err"]}`)
+        return;
+    }
+    resultArray = rePopulate(JSON.parse(returnStruct["sudoku"]), 3);
+    populateTab(roomResultList, resultArray);
     e.preventDefault();
 }
+///////////////////////////////////////
+// The generate table population functions
+// Live below here
+///////////////////////////////////////
+
 
 function clearTable(element) {
     while(element.firstChild) {
@@ -85,4 +144,40 @@ function rePopulate(table, length){
         resultTable.push(table.slice(i, i+length));
     }
     return resultTable
+}
+
+
+function inputTablePopulate(element, x, y, label, type){
+    for (let i=0; i<y; i++){
+        let row = document.createElement('tr')
+        for (let j=0;j<x;j++){
+            ip = document.createElement('input')
+            ip.id = `${label}__input__${j}__${i}`;
+            ip.type = type
+            td = document.createElement('td')
+            td.appendChild(ip)
+            row.appendChild(td)
+        }
+        element.appendChild(row)
+    }
+}
+
+function inputTableRetrieve(element){
+    let returnTable = Array();
+    // let alerted = false;
+    for (let row = element.firstChild; row!=null; row=row.nextElementSibling) {
+        console.log(`Got a new row ${row}`);
+        let rowEntry = Array();
+        for (let cel=row.firstChild; cel!=null; cel=cel.nextElementSibling){
+            console.log(`got a new column ${cel}`);
+            const contents = cel.firstChild.value;
+            // if (!alerted && (contents === "")){
+            //     alert(`Cell has null contents`);
+            //     alerted = true
+            // }
+            rowEntry.push(contents);
+        }
+        returnTable.push(rowEntry);
+    }
+    return returnTable;
 }
--- static/puzzle/main.go
+++ static/puzzle/main.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"syscall/js"
+	"strconv"
 
 	"github.com/cbehopkins/wordlist"
 	"github.com/cbehopkins/ana"
+	cntSlv "github.com/cbehopkins/countdown/cnt_slv"
+
 )
 
 func anagram(this js.Value, i []js.Value) interface{} {
@@ -45,8 +48,110 @@ func anagram(this js.Value, i []js.Value) interface{} {
 	}
 }
 
+func jsonToArrayInt(inputText string) (retArray []int, err error){
+	var inputValues []string
+	err = json.Unmarshal([]byte(inputText), &inputValues)
+	if err != nil {
+		return nil, err
+	}
+	retArray = make([]int, len(inputValues))
+	for i, v := range inputValues {
+		val, err := strconv.Atoi(v)
+		if err != nil{
+			return nil, err
+		}
+		retArray[i] = val
+	}
+	return retArray, nil
+}
+func jsonTo2dArrayInt(inputText string) (retArray [][]int, err error){
+	var inputValues [][]string
+	err = json.Unmarshal([]byte(inputText), &inputValues)
+	if err != nil {
+		return nil, err
+	}
+	retArray = make([][]int, len(inputValues))
+	for j, rowIn := range inputValues {
+		rowOut := make([]int, len(rowIn))
+		for i, v := range rowIn {
+			val, err := strconv.Atoi(v)
+			if err != nil{
+				return nil, err
+			}
+			rowOut[i] = val
+		}
+		retArray[j] = rowOut
+	}
+	return retArray, nil
+}
+func RunCountdown(target int, sources []int) string {
+		foundValues := cntSlv.NewNumMap()
+		//found_values.SelfTest = true
+		foundValues.UseMult = true
+		foundValues.PermuteMode = cntSlv.FastMap
+		foundValues.SeekShort = false // TBD make this controllable
+
+		fmt.Println("Starting permute")
+		returnProofs := foundValues.CountHelper(target, sources)
+		for range returnProofs {
+			//fmt.Println("Proof Received", v)
+		}
+		//fmt.Println("Permute Complete", proof_list)
+		return foundValues.GetProof(target)
+
+}
+func countdown(this js.Value, i []js.Value) interface{} {
+	fmt.Println("Here we are in countdown")
+	if len(i) != 2 {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error len of input wrong", len(i))}
+	}
+
+	if i[0].Type() != js.TypeString {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error, wrong type", i[0].Type)}
+	}
+
+	if i[1].Type() != js.TypeString {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error, wrong type", i[1].Type)}
+	}
+
+	target, err := strconv.Atoi(i[0].String())
+	if err != nil {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error, Target int parse fail", err)}
+	}
+	fmt.Println("We're being asked to target:", target)
+	fmt.Println("Our JASON input is:", i[1].String())
+	inputValues, err := jsonToArrayInt(i[1].String())
+	if err != nil {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error, input values int parse fail", err)}
+	}
+	fmt.Println("We have the inputs:", inputValues)
+	return map[string]interface{}{
+		"error":    "",
+		"countdown": RunCountdown(target, inputValues),
+	}
+}
+func sudoku(this js.Value, i []js.Value) interface{} {
+	fmt.Println("Here we are in sudoku")
+	if len(i) != 1 {
+		return map[string]interface{}{
+			"error": fmt.Sprint("Error len of input wrong", len(i))}
+	}
+
+
+
+	return map[string]interface{}{
+		"error":    "",
+	}
+}
 func registerCallbacks() {
 	js.Global().Set("anagram", js.FuncOf(anagram))
+	js.Global().Set("countdown", js.FuncOf(countdown))
+	js.Global().Set("sudoku", js.FuncOf(sudoku))
 
 }
 
